fbfa1fa487e70b9ee5799d78a7455c425ab9d223
name <moesanaye.ptuec@gmail.com>
2021-10-01 11:47:17 +0630

    playing game

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>How fast can u catch me?</h1>   
+    <button @click="start" :disabled="isPlaying">play</button> 
+    <!--step 3-->
+    <div v-if="isPlaying">
+      <!--props-->
+      <Block :delay = "delay" @endGame="endGame"/>
+    </div>
+    <div v-if="end">
+       <Result :score="score"/>
+    </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// step 1 do import
+import Block from "./components/Block.vue"
+import Result from "./components/Result.vue"
 export default {
   name: 'App',
+  // step 2 register
   components: {
-    HelloWorld
+    Block,
+    Result
+  },
+  data(){
+    return{
+      isPlaying: false,
+      delay:null,
+      score:0,
+      end:false
+    }
+  },
+  methods:{
+    start(){
+      this.isPlaying=true;
+      this.delay=2000+Math.random()*5000;
+    },
+    endGame(score){
+      this.score=score;
+      this.isPlaying=false;
+      this.end = true
+    }
   }
 }
 </script>
